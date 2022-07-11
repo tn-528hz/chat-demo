c5d8f5f3d3db66d9926e8307776407467b9e32e1
tn-create1112 <0ak792332w3227j@au.com>
2022-07-11 16:10:38 +0900

    first commit

---
+++ config/asgi.py
@@ -0,0 +1,14 @@
+import os
+import django
+from channels.auth import AuthMiddlewareStack
+from channels.http import AsgiHandler
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
+
+application = ProtocolTypeRouter({
+     'http': AsgiHandler,
+     'websocket': AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns)),
+})
